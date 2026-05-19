c6e57c704f3a9b8ed7618ce1a8b439a47334692d
dieto <dietoco92@hotmail.com>
2026-05-19 19:07:31 +0200

    Script SQL para creación de la base de datos

---
+++ squema.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: refugiodb
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cuidadores`
+--
+
+DROP TABLE IF EXISTS `cuidadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cuidadores` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(100) NOT NULL,
+  `Telefono` varchar(20) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuidadores`
+--
+
+LOCK TABLES `cuidadores` WRITE;
+/*!40000 ALTER TABLE `cuidadores` DISABLE KEYS */;
+INSERT INTO `cuidadores` VALUES (1,'Carlos Gómez','600112233'),(2,'María López','600445566');
+/*!40000 ALTER TABLE `cuidadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mascotas`
+--
+
+DROP TABLE IF EXISTS `mascotas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mascotas` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Especie` varchar(30) NOT NULL,
+  `Edad` int NOT NULL,
+  `EstadoAdopcion` varchar(20) DEFAULT 'Disponible',
+  `FechaIngreso` datetime DEFAULT CURRENT_TIMESTAMP,
+  `CuidadorId` int DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_Mascotas_Cuidadores` (`CuidadorId`),
+  CONSTRAINT `FK_Mascotas_Cuidadores` FOREIGN KEY (`CuidadorId`) REFERENCES `cuidadores` (`Id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mascotas`
+--
+
+LOCK TABLES `mascotas` WRITE;
+/*!40000 ALTER TABLE `mascotas` DISABLE KEYS */;
+INSERT INTO `mascotas` VALUES (1,'Rocky','Perro',3,'Disponible','2026-05-19 10:39:02',1),(2,'Luna','Gato',1,'Disponible','2026-05-19 10:39:02',1),(3,'Zeus','Perro',4,'Disponible','2026-05-19 11:13:22',2);
+/*!40000 ALTER TABLE `mascotas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-19 19:04:15
